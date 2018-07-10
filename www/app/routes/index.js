a133589a6c5eb2701d4ef5af017cf5a14a757d6d
vadimvlk <40887689+vadimvlk@users.noreply.github.com>
2018-07-10 18:15:27 +0700

    Update index.js

--- www/app/routes/index.js
+++ www/app/routes/index.js
@@ -15,21 +15,21 @@ export default Ember.Route.extend({
 
             var result = '<div class="col-md-12"><br/><div class="note note-info">';
             result += '<span>';
-            result += '<p>Расчет для скорости <span class="label label-success">' + hashRate + '</span> MH/s и сложности <span class="label label-success">' +  data.nodes[0].difficulty + '</span>:</p>';
+            result += '<p>Estimation of profits for <span class="label label-success">' + hashRate + '</span> MH/s and difficulty  <span class="label label-success">' +  data.nodes[0].difficulty + '</span>:</p>';
 
             result += '<dl class="dl-horizontal">';
             var intervals = [1, 3, 7, 14, 30];
-            var ppsRate = 3 * (1 - config.APP.PoolFee / 100) / data.nodes[0].difficulty;
+            var ppsRate = 2 * (1 - config.APP.PoolFee / 100) / data.nodes[0].difficulty;
             for (var i = 0; i < intervals.length; ++i) {
                 var seconds = intervals[i] * 86400;
                 var hashes = hashRate * Math.pow(10, 6) * seconds;
-                result += '<dt>' + intervals[i] + 'дн.</dt>';
-                result += '<dd>' + (hashes * ppsRate).toFixed(8)  +  ' ETH</dd>';
+                result += '<dt>' + intervals[i] + 'd.</dt>';
+                result += '<dd>' + (hashes * ppsRate).toFixed(8)  +  ' Moac</dd>';
             }
 
             result += '</dl>';
             result += '</span>';
-            result += '<p>Оценка точна только для PPS режима. В HVPPS режиме будут отклонения в обе стороны из-за персональной удачи майнера, особенно на коротких дистанциях.</p>';
+            result += '<p>Estimation is accurate for PPS.</p>';
             result += '</div></div>';
 
             document.getElementById('calculation-result').innerHTML = result;
